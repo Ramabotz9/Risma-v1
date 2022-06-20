84cac7aba4dc0c5fc6a9785dff63d6f31ead01b3
OscarbotzMD <107636164+OscarbotzMD@users.noreply.github.com>
2022-06-21 01:57:07 +0700

    Update main-thanks.js

--- plugins/main-thanks.js
+++ plugins/main-thanks.js
@@ -14,7 +14,7 @@ hyzer = {
         }
        } 
      }
-conn.sendImageAsSticker(m.chat, 'https://telegra.ph/file/4c97e4a74649720aaa120.jpg', hyzer, { packname: "sticker by", author: "ArullOfc" })
+conn.sendImageAsSticker(m.chat, 'https://telegra.ph/file/4c97e4a74649720aaa120.jpg', hyzer, { packname: "sticker by", author: "JarotOfc" })
 }
 
 handler.customPrefix = /^(Terima Kasih|Thanks|tq)$/i
