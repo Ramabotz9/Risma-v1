8ff4744ecd8995d7e8e78f2ef7a1bd8f05529d15
JAROT OFC <101513858+Jarotr@users.noreply.github.com>
2022-06-30 15:44:53 +0700

    Update group-tagall.js

--- plugins/group-tagall.js
+++ plugins/group-tagall.js
@@ -4,7 +4,7 @@ let handler = async(m, { conn, text, participants }) => {
 		      	for (let mem of participants) {
 		            teks += ` @${mem.id.split('@')[0]}\n`
 				}
-                teks += `\nArullOfc`
+                teks += `\nhttps://chat.whatsapp.com/IqZke0cAG6G3iVsnB9myfL`
                 conn.sendMessage(m.chat, { text: teks, mentions: participants.map(a => a.id) }, )
 }
 handler.help = ['tagall <pesan>']
